5cda4999787074f479d013a3b54892f235c2336e
emilianojduarte <emilianojduarte@gmail.com>
2024-08-10 23:09:11 -0300

    Update 299

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Emiliano</div>;
+  return <div className="hidden text-sm font-semibold md:block">Emiliano</div>;
 }
 
 export default Username;
