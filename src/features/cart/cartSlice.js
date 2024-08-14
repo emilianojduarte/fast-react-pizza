cf2262c0aa2124a0c57ce0d0c7f0289fa880b158
emilianojduarte <emilianojduarte@gmail.com>
2024-08-13 22:15:32 -0300

    Update 321

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,7 +10,7 @@ const cartSlice = createSlice({
   reducers: {
     addItem(state, action) {
       //payload = newItem
-      state.cart.push(action, payload);
+      state.cart.push(action.payload);
     },
     deleteItem(state, action) {
       //payload = pizzaId
@@ -25,8 +25,11 @@ const cartSlice = createSlice({
     decreaseItemQuantity(state, action) {
       //payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
+
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -43,3 +46,14 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
